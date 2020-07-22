2c01cba2c93b0c64261a7696bb28cc1568c324c1
Yash Suvidh Kankariya <yashk12345@gmail.com>
2020-07-22 19:47:26 -0400

    Adding Sign in page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/home-page/home-page.component';
 import LandingPage from './pages/landing-page/landing-page.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-page.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import UserPage from './pages/user-page/user-page.component';
 
 
@@ -12,7 +12,7 @@ const App = () => (
     <Switch>
       <Route exact path='/' component={ HomePage } />
       <Route exact path='/landing' component={ LandingPage } />
-      <Route exact path='/sign-in' component={ SignInAndSignUpPage } />
+      <Route exact path='/signin' component={ SignInAndSignUpPage } />
       <Route exact path='/user' component={ UserPage } />
     </Switch>
   </div>
