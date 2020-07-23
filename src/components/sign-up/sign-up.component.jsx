3f44209541520950857b1c35a7838eff5c37f29e
Michael <59486087+michaelkong0@users.noreply.github.com>
2020-07-23 11:01:25 -0400

    Change signup
    Returned to old formatting, double check for other devices

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -62,9 +62,10 @@ class SignUp extends React.Component {
     const { displayName, email, password, confirmPassword } = this.state;
     return (
       <div className='sign-up'>
-        <h2 className='title'>I do not have a account</h2>
-        <span>Sign up with your email and password</span>
+        
         <form className='sign-up-form' onSubmit={this.handleSubmit} autoComplete='off'>
+          <h2 className='title'>I do not have a account</h2>
+          <span>Sign up with your email and password</span>
           <FormInput
             type='text'
             name='displayName'
