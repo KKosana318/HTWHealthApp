22d31c27afa80cdc4e430c8da424bd5354b120fc
Wesley Shen <wesleyshen070@gmail.com>
2020-07-22 23:29:42 -0400

    no idea

--- src/components/patient-profile/patient-profile.component.jsx
+++ src/components/patient-profile/patient-profile.component.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './form-input.styles.css';
-
 export const PatientProfile = props => (
     <div className = 'pprofile-container'>
         <Link className='option' to='/home'>
