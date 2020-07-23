142a33b7ffe5d5f21e4e9c15b7786716ad6ff067
Yash Suvidh Kankariya <yashk12345@gmail.com>
2020-07-22 20:58:24 -0400

    google firebase added

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import {signInWithGoogle} from '../../firebase/firebase.utils';
 
 import './sign-in.styles.js'
 
@@ -48,7 +51,9 @@ class SignIn extends React.Component{
                     required />
                     <label>Password</label>
 
-                    <input type="submit" value='Submit Form'/>
+                    <CustomButton type="submit"> Sign In </CustomButton>
+                    <CustomButton onClick={signInWithGoogle}> Sign in with Google </CustomButton>
+
                 </form>
             </div>
         );
