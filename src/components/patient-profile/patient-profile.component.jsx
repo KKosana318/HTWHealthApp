9eff4b648e3dfc22bc55007bdd980db7be98976e
Wesley Shen <wesleyshen070@gmail.com>
2020-07-23 00:10:48 -0400

    change profile

--- src/components/patient-profile/patient-profile.component.jsx
+++ src/components/patient-profile/patient-profile.component.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const PatientProfile = props => (
-    <div className = 'pprofile-container'>
+    <div className = 'patient-profile-container'>
         <Link className='option' to='/home'>
         <img 
             alt="patient" 
-            src={`https://robohash.org/${props.patient.id}?set=set2&size=180x180`} 
+            src={`https://robohash.org/${props.patient.id}?set=set2&size=180x180`}
         />
         </Link>
     </div>
