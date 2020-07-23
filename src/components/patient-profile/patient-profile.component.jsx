edb8080bac7cd9cf5bf913d0d2d6d47297559552
Wesley Shen <wesleyshen070@gmail.com>
2020-07-22 22:13:07 -0400

    change patient-profile form

--- src/components/patient-profile/patient-profile.component.jsx
+++ src/components/patient-profile/patient-profile.component.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './form-input.styles.css';
 
 export const PatientProfile = props => (
     <div className = 'pprofile-container'>
+        <Link className='option' to='/home'>
         <img 
             alt="patient" 
             src={`https://robohash.org/${props.patient.id}?set=set2&size=180x180`} 
         />
-        <h2>{props.patient.name}</h2>
-        <h2>{props.patient.severity}</h2>
+        </Link>
     </div>
 )
