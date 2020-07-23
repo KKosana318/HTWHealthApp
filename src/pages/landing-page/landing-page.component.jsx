f0dfa157da86a8ac81c1e17fef7c76b978243bab
Kesav Kosana <kkosana33@gmail.com>
2020-07-22 21:50:03 -0400

    add stuff

--- src/pages/landing-page/landing-page.component.jsx
+++ src/pages/landing-page/landing-page.component.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const LandingPage = () => (
-  <div>Landing Page</div>
+  <div>
+    <h1>Join a Global Shift in Healthcare Access</h1>
+    <h2>Use our technology to bring your medical expertise to communities that lack access to immediate healthcare.</h2>
+  </div>
 );
 
 export default LandingPage;
