e2dd78c39a3cd76d19f0405e1fbb09bba929517a
Kesav Kosana <kkosana33@gmail.com>
2020-07-24 01:59:18 -0400

    add homepage and remove patient page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/home-page/home-page.component';
-import PatientPage from './pages/patient-page/patient-page.component';
 import LandingPage from './pages/landing-page/landing-page.component';
 import TestPage from './pages/test-page/test-page.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
@@ -63,7 +62,6 @@ class App extends React.Component {
           <Route exact path='/landing' component={ LandingPage } />
           <Route exact path='/signin' component={ SignInAndSignUpPage } />
           <Route exact path='/user' component={ UserPage } />
-          <Route exact path='/patient' component={ PatientPage } />
           <Route exact path='/test' component={ TestPage } />
           <Route exact path='/video' component={ VideoPage } />
         </Switch>
@@ -72,5 +70,4 @@ class App extends React.Component {
     );
   }
 }
-
 export default App;
