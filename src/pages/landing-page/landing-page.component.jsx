052cc111a8971b0168077d400b3a8de96756511e
Michael <59486087+michaelkong0@users.noreply.github.com>
2020-07-24 01:46:34 -0400

    Change images

--- src/pages/landing-page/landing-page.component.jsx
+++ src/pages/landing-page/landing-page.component.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import * as Scroll from 'react-scroll';
 //eslint-disable-next-line
 import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
-
+import heart from '../../images/heart.png';
+import bed from '../../images/bed.png';
+import call from '../../images/call.png';
 import TextImageModule from '../../components/text-image-module/text-image-module.component';
 
 import { LandingPageContainer, TopModule, TextContainer, FeaturesContainer, GetStartedContainer, GetStartedButton } from './landing-page.styles';
@@ -19,9 +21,9 @@ const LandingPage = () => (
       </TextContainer>
     </TopModule>
     <FeaturesContainer>
-      <TextImageModule name="test1" image={ 'https://images.vexels.com/media/users/3/136170/isolated/preview/1a0fc726567fe21282676126358b795d-heart-disease-logo-by-vexels.png' } title={ 'Remotely diagnose health conditions' } content={ 'Use comprehensive data provided by patients in rural and low-access areas to remotely diagnose health conditions.' } />
-      <TextImageModule image={ 'https://www.nicepng.com/png/full/423-4231033_medical-clipart-emergency-medicine-emergency-room-nurse-clipart.png' } title={ 'Address health emergencies' } content={ 'Respond to patients in emergency situations by suggesting treatment options for communities with limited equipment with help from our health analysis AI tools.' } />
-      <TextImageModule image={ 'https://static.thenounproject.com/png/82045-200.png' } title={ 'Check in regularly with patients over video' } content={ 'Use our built-in video conferencing capabilities to chat live with patients for regular check-ups, answering health questions, and addressing minor health concerns.' } />
+      <TextImageModule name="test1" image={heart  } title={'Remotely diagnose health conditions'} content={ 'Use comprehensive data provided by patients in rural and low-access areas to remotely diagnose health conditions.' } />
+      <TextImageModule image={ bed } title={ 'Address health emergencies' } content={ 'Respond to patients in emergency situations by suggesting treatment options for communities with limited equipment with help from our health analysis AI tools.' } />
+      <TextImageModule image={ call } title={ 'Check in regularly with patients over video' } content={ 'Use our built-in video conferencing capabilities to chat live with patients for regular check-ups, answering health questions, and addressing minor health concerns.' } />
     </FeaturesContainer>
     <GetStartedContainer>
       <h1>Get started today</h1>
