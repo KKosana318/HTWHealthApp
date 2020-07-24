fee5b48d3eae58251eda699dc3eda7c1f790863e
Yash Suvidh Kankariya <yashk12345@gmail.com>
2020-07-24 13:36:49 -0400

    L

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import LandingPage from './pages/landing-page/landing-page.component';
 import TestPage from './pages/test-page/test-page.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import UserPage from './pages/user-page/user-page.component';
-
+import patientsubmit from './pages/submit-information/patient-sumbit.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 //import firebase from './firebase/firebase.utils';
