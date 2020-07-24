1dc42a1c4b50b8f03706319d9bcf487796c58a65
Jay <jaykul00@gmail.com>
2020-07-24 12:32:39 -0400

    updating

--- src/pages/video-page/video-page.component.jsx
+++ src/pages/video-page/video-page.component.jsx
@@ -4,6 +4,36 @@ import './video-page.styles.css';
 
 const VideoPage = () => (
   <div>jay kys</div>
-);
+//   opentok.createSession(function(err, session) {
+//     if (err) return console.log(err);
+//
+//     // save the sessionId
+//     db.save('session', session.sessionId, done);
+//   });
+//
+//   // The session will the OpenTok Media Router:
+//   opentok.createSession({mediaMode:"routed"}, function(err, session) {
+//     if (err) return console.log(err);
+//
+//     // save the sessionId
+//     db.save('session', session.sessionId, done);
+//   });
+//
+//   // A Session with a location hint
+//   opentok.createSession({location:'12.34.56.78'}, function(err, session) {
+//     if (err) return console.log(err);
+//
+//     // save the sessionId
+//     db.save('session', session.sessionId, done);
+//   });
+//
+//   // A Session with an automatic archiving
+//   opentok.createSession({mediaMode:'routed', archiveMode:'always'}, function(err, session) {
+//     if (err) return console.log(err);
+//
+//     // save the sessionId
+//     db.save('session', session.sessionId, done);
+//   });
+// );
 
 export default VideoPage;
