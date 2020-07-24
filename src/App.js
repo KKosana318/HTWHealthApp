722b19ae61d84461c1ffa4d08cb4f48ab2fcba0f
Jay <jaykul00@gmail.com>
2020-07-24 16:14:59 -0400

    video works

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import TestPage from './pages/test-page/test-page.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import UserPage from './pages/user-page/user-page.component';
 import VideoPage from './pages/video-page/video-page.component';
+import { OTSession, OTStreams, preloadScript } from 'opentok-react';
+import ConnectionStatus from './components/ConnectionStatus';
+import Publisher from './components/Publisher';
+import Subscriber from './components/Subscriber';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
@@ -19,16 +23,23 @@ import Header from './components/Header/header.component';
 
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       currentUser: null
     }
+    this.sessionEvents = {
+    sessionConnected: () => {
+      this.setState({ connected: true });
+    },
+    sessionDisconnected: () => {
+      this.setState({ connected: false });
+    }
   }
+}
 
   unsubscribeFromAuth = null
-
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -57,7 +68,7 @@ class App extends React.Component {
     return (
 
       <div className='full-site-content'>
-        <Route path='/' component={ (props) => (<Header routeProps={ props } currentUser={ this.state.currentUser } />) } />
+        <Route path='/' component={ (props) => (<Header routeProps={ props } currentUser ={ this.state.currentUser } />) } />
         <Switch>
           <Route exact path='/' component={ HomePage } />
           <Route exact path='/landing' component={ LandingPage } />
@@ -65,7 +76,7 @@ class App extends React.Component {
           <Route exact path='/user' component={ UserPage } />
           <Route exact path='/patient' component={ PatientPage } />
           <Route exact path='/test' component={ TestPage } />
-          <Route exact path='/video' component={ VideoPage } />
+          <Route exact path='/video' component={ () => (<VideoPage apiKey ={this.props.apiKey} sessionId ={this.props.sessionId} token ={this.props.token}/>) } />
         </Switch>
 
       </div>
--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Config from './config';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <App
+    apiKey={Config.API_KEY}
+    sessionId={Config.SESSION_ID}
+    token={Config.TOKEN}/>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
