12eb6c5a76147d41cadae1864f208fe90d4a95e5
Wesley Shen <wesleyshen070@gmail.com>
2020-07-24 14:35:37 -0400

    Update video-page.component.jsx

--- src/pages/video-page/video-page.component.jsx
+++ src/pages/video-page/video-page.component.jsx
@@ -3,41 +3,7 @@ import React from 'react';
 import './video-page.styles.css';
 
 const VideoPage = () => (
-<<<<<<< HEAD
-  <div>jay kys</div>
-  // opentok.createSession(function(err, session) {
-  //   if (err) return console.log(err);
-  //
-  //   // save the sessionId
-  //   db.save('session', session.sessionId, done);
-  // });
-  //
-  // // The session will the OpenTok Media Router:
-  // opentok.createSession({mediaMode:"routed"}, function(err, session) {
-  //   if (err) return console.log(err);
-  //
-  //   // save the sessionId
-  //   db.save('session', session.sessionId, done);
-  // });
-  //
-  // // A Session with a location hint
-  // opentok.createSession({location:'12.34.56.78'}, function(err, session) {
-  //   if (err) return console.log(err);
-  //
-  //   // save the sessionId
-  //   db.save('session', session.sessionId, done);
-  // });
-  //
-  // // A Session with an automatic archiving
-  // opentok.createSession({mediaMode:'routed', archiveMode:'always'}, function(err, session) {
-  //   if (err) return console.log(err);
-  //
-  //   // save the sessionId
-  //   db.save('session', session.sessionId, done);
-  // });
-=======
   <div>jay is ugly</div>
->>>>>>> 85133818a7e6d9bf8fdb7078902a8d7ea1c96272
 );
 
 export default VideoPage;
