f0e8b54dcdc179775e79ce35f638a2543d1f5813
Kesav Kosana <kkosana33@gmail.com>
2020-07-24 14:05:47 -0400

    make video page class comp

--- src/pages/video-page/video-page.component.jsx
+++ src/pages/video-page/video-page.component.jsx
@@ -2,38 +2,20 @@ import React from 'react';
 
 import './video-page.styles.css';
 
-const VideoPage = () => (
-  <div>jay kys</div>
-//   opentok.createSession(function(err, session) {
-//     if (err) return console.log(err);
-//
-//     // save the sessionId
-//     db.save('session', session.sessionId, done);
-//   });
-//
-//   // The session will the OpenTok Media Router:
-//   opentok.createSession({mediaMode:"routed"}, function(err, session) {
-//     if (err) return console.log(err);
-//
-//     // save the sessionId
-//     db.save('session', session.sessionId, done);
-//   });
-//
-//   // A Session with a location hint
-//   opentok.createSession({location:'12.34.56.78'}, function(err, session) {
-//     if (err) return console.log(err);
-//
-//     // save the sessionId
-//     db.save('session', session.sessionId, done);
-//   });
-//
-//   // A Session with an automatic archiving
-//   opentok.createSession({mediaMode:'routed', archiveMode:'always'}, function(err, session) {
-//     if (err) return console.log(err);
-//
-//     // save the sessionId
-//     db.save('session', session.sessionId, done);
-//   });
-// );
+class VideoPage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        TokBox
+      </div>
+    )
+  }
+}
 
 export default VideoPage;
